c086b9c539ed832c4d07444b70d7b95184391e90
Barbara <40583024+barbaraguarino@users.noreply.github.com>
2026-06-26 12:06:10 -0300

    Criando o endpoint

--- src/main/java/br/com/myshelf/backend/infrastructure/web/controller/AuthController.java
+++ src/main/java/br/com/myshelf/backend/infrastructure/web/controller/AuthController.java
@@ -1,11 +1,16 @@
 package br.com.myshelf.backend.infrastructure.web.controller;
 
+import br.com.myshelf.backend.application.dto.UserLoginDTO;
+import br.com.myshelf.backend.application.dto.UserLoginResponseDTO;
 import br.com.myshelf.backend.application.dto.UserRegisterDTO;
 import br.com.myshelf.backend.application.service.AuthService;
 import jakarta.validation.Valid;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,9 +24,34 @@ public class AuthController {
 
     private final AuthService authService;
 
+    @Value("${api.security.token.name}")
+    private String TOKEN_NAME;
+
     @PostMapping("/register")
     public ResponseEntity<Void> register(@Valid @RequestBody UserRegisterDTO userRegisterDTO) {
         authService.createUser(userRegisterDTO);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<UserLoginResponseDTO> login(
+            @Valid @RequestBody UserLoginDTO userLoginDTO) {
+
+        var response = authService.login(userLoginDTO);
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .header(HttpHeaders.SET_COOKIE, getResponseCookie(response.token()).toString())
+                .body(response.dto());
+
+    }
+
+    private ResponseCookie getResponseCookie(String token) {
+        return ResponseCookie.from(TOKEN_NAME, token)
+                .httpOnly(true)
+                .secure(false)
+                .sameSite("Strict")
+                .path("/")
+                .maxAge(86400)
+                .build();
     }
 }
