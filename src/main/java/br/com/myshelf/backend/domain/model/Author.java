789024cfde4337e7b28707307cd5f86ec968cc2c
Barbara <40583024+barbaraguarino@users.noreply.github.com>
2026-06-28 12:45:20 -0300

    Tabela no banco de dados e entidade

---
+++ src/main/java/br/com/myshelf/backend/domain/model/Author.java
@@ -0,0 +1,38 @@
+package br.com.myshelf.backend.domain.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_authors")
+@Getter
+@Builder(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String name;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private Timestamp createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Timestamp updatedAt;
+
+    public static Author createAuthor(String name) {
+        return Author.builder()
+                .name(name.trim())
+                .build();
+    }
+}
